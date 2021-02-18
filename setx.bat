8b84a898129cee4cc2be7c7aca80d3f10c805edf
1tyagiji <30944675+1tyagiji@users.noreply.github.com>
2021-02-18 14:12:15 +0530

    Initial commit

---
+++ setx.bat
@@ -0,0 +1,15 @@
+setx DERBY_HOME C:\hadoop\derby
+setx HADOOP_HOME C:\hadoop
+setx HADOOP_USER_CLASSPATH_FIRST true
+setx HIVE_HOME C:\hadoop\hive
+setx HIVE_BIN C:\hadoop\hive\bin
+setx HIVE_LIB C:\hadoop\hive\lib
+setx JAVA_HOME C:\Java\jdk1.8.0_281
+setx PIG_HOME C:\hadoop\pig
+setx SQOOP_HOME C:\hadoop\sqoop
+mkdir C:\hadoop\data
+mkdir C:\hadoop\data\dfs
+mkdir C:\hadoop\data\dfs\data_330
+mkdir C:\hadoop\data\dfs\namespace_logs_330
+mkdir C:\cygdrive
+mklink /J  C:\cygdrive\c\ C:\
